27c645cb3bcfc3a84f1f7a1f3822fe3e63d036ad
Olaf Tomalka <olaf@tomalka.me>
2024-06-15 17:54:33 +0200

    Add config page

---
+++ src/app/config/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Flex, VStack } from "@chakra-ui/layout";
+import React from "react";
+
+export default function Page() {
+  return (
+    <Flex alignItems="center" justifyContent="center" w="100vw" h="100vh">
+      <VStack>
+        <FormControl>
+          <FormLabel>Matching funds</FormLabel>
+          <Input value="0" />
+        </FormControl>
+        <Button colorScheme="blue">Update</Button>
+      </VStack>
+    </Flex>
+  );
+}
