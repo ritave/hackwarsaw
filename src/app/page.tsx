4b4618b9298d1f8d25063876a58b153b9244ae6f
Olaf Tomalka <olaf@tomalka.me>
2024-06-15 19:14:43 +0200

    use search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,66 @@
 "use client";
-import {Ngov, Params as NgovParams} from "../components/ngov";
+import { Ngov, Params as NgovParams } from "../components/ngov";
 import {
-    Grid,
-    GridItem,
-    HStack,
-    Input,
-    SimpleGrid,
-    Spacer,
+  Grid,
+  GridItem,
+  HStack,
+  Input,
+  SimpleGrid,
+  Spacer,
 } from "@chakra-ui/react";
 import Fuse from "fuse.js";
-import {useState} from "react";
-import {MockNonGovService} from "@/services/NonGovService";
+import { useState } from "react";
+import { MockNonGovService } from "@/services/NonGovService";
 import React from "react";
-import {Login} from "../components/login";
+import { Login } from "../components/login";
 
 const nonGovSrv = new MockNonGovService();
 
 export default function Page() {
-    const [ngovs, setNgovs] = useState(nonGovSrv.list(""));
+  const [ngovs, setNgovs] = useState(nonGovSrv.list(""));
+  const [search, setSearch] = useState("");
 
-    const updateSearchData = (filter: string) => {
-        let ngovs = nonGovSrv.list(filter)
-        setNgovs(ngovs)
-    };
+  const updateSearchData = (filter: string) => {
+    setSearch(filter);
+    let ngovs = nonGovSrv.list(filter);
+    setNgovs(ngovs);
+  };
 
-    return (
-        <Grid
-            templateAreas={`
+  return (
+    <Grid
+      templateAreas={`
       "filter"
       "ngovs"
     `}
-            m={4}
-            gap={4}
-        >
-            <GridItem area="filter">
-                <HStack spacing={4}>
-                    <Input
-                        placeholder="Filter"
-                        size="sm"
-                        maxWidth={400}
-                        onChange={(e) => updateSearchData(e.target.value)}
-                    />
-                    <Spacer/>
-                    <Login/>
-                </HStack>
-            </GridItem>
-            <GridItem area={"ngovs"}>
-                <SimpleGrid columns={5} spacing={4}>
-                    {ngovs.map(({contributors, name, krs, totalRaised}) => (
-                        <Ngov
-                            contributors={contributors}
-                            krs={krs}
-                            name={name}
-                            totalRaised={totalRaised}
-                            key={krs}
-                        />
-                    ))}
-                </SimpleGrid>
-            </GridItem>
-        </Grid>
-    );
+      m={4}
+      gap={4}
+    >
+      <GridItem area="filter">
+        <HStack spacing={4}>
+          <Input
+            placeholder="Filter"
+            size="sm"
+            maxWidth={400}
+            value={search}
+            onChange={(e) => updateSearchData(e.target.value)}
+          />
+          <Spacer />
+          <Login />
+        </HStack>
+      </GridItem>
+      <GridItem area={"ngovs"}>
+        <SimpleGrid columns={5} spacing={4}>
+          {ngovs.map(({ contributors, name, krs, totalRaised }) => (
+            <Ngov
+              contributors={contributors}
+              krs={krs}
+              name={name}
+              totalRaised={totalRaised}
+              key={krs}
+            />
+          ))}
+        </SimpleGrid>
+      </GridItem>
+    </Grid>
+  );
 }
